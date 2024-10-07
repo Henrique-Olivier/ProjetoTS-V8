928dec7a128ca3e7d3bee9f5c927a0e206e24217
Matheus Castro <castro.dev37@gmail.com>
2024-10-07 10:29:02 -0300

    adicionando validações no modal

--- src/manager.ts
+++ src/manager.ts
@@ -1,8 +1,26 @@
+import { getCategories } from "./getCategorie";
 import { getProducts } from "./getProducts";
+import { showCategories } from "./showCategories";
 import { showEmptyState } from "./showEmptyState";
 import { showProducts } from "./showProducts";
 
-const insertProducts: HTMLElement = document.querySelector("#insert-products")!;
+type htmlInput = HTMLInputElement;
+type htmlElement = HTMLElement;
+
+const insertProducts: htmlElement = document.querySelector("#insert-products")!;
+const btnAddProduct: htmlElement = document.querySelector("#btn-add-product")!;
+const inputProductName: htmlInput = document.querySelector("#input-name")!;
+const textProductResume: htmlInput = document.querySelector("#text-resume")!;
+const selectProductCategory: htmlInput = document.querySelector("#select-category")!;
+const alertModal: htmlElement = document.querySelector("#alert-modal")!;
+
+btnAddProduct.addEventListener("click", newProduct);
+
+const categories = await getCategories();
+
+if (categories !== null ) {
+    showCategories(categories, selectProductCategory);
+}
 
 const produtos = await getProducts();
 
@@ -12,3 +30,46 @@ if (produtos !== null ) {
     showEmptyState();
 }
 
+function newProduct() {
+    const isNameValid = validateInputName(inputProductName.value);
+    const isMinResumeValid = validateMinTextResume(textProductResume.value);
+    const isMaxResumeValid = validateMinTextResume(textProductResume.value);
+    const isCategoryValid = validateSelectCategory(selectProductCategory.value);
+
+    if(!isNameValid) {
+        return showAlert(alertModal, "Digite um nome com mais de 3 caracteres");
+    }
+
+    if(!isMinResumeValid) {
+        return showAlert(alertModal, "O resume não deve ficar vazio")
+    }
+
+    if(!isMaxResumeValid) {
+        return showAlert(alertModal, "O resume não pode ter mais de 100 caracteres")
+    }
+
+    if(!isCategoryValid) {
+        return showAlert(alertModal, "Escolha uma categoria válida")
+    }
+}
+
+function validateInputName(inputName: string) {
+    return inputName.length > 3;
+}
+
+function validateMinTextResume(textResume: string) {
+    return textResume.length > 0;
+}
+
+function validateMaxTextResume(textResume: string) {
+    return textResume.length <= 100;
+}
+
+function validateSelectCategory(selectCategory: string) {
+    return selectCategory !== "Todas"
+}
+
+function showAlert(element: HTMLElement, message: string) {
+    element.classList.remove("d-none")
+    element.textContent = message;
+}
